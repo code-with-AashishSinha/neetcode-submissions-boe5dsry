c45b169235cce4fc193c897563fc74f3670ace03
code-with-AashishSinha <sinhaaashishkumar63@gmail.com>
2026-04-08 08:02:49 +0530

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+       
+        //step 1: reverse whole array...
+        //step 2: reverse the element on to 0 to k-1..
+        //step 3: reverse the remaining element in the array...
+        int i=0;
+        int j=nums.length-1;
+        k=k%nums.length;
+        output(nums, i, j);
+        output(nums, i, k-1);
+        output(nums,k,j);
+    }
+    public  int[] output(int[]nums,int i,int j){
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        return nums;
+    }
+    
+}
